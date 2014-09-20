1eac41252e867aece4cb229986ac64acb61d6a30
Doctor- <413Calliope413@gmail.com>
2014-09-20 11:26:08 -0700

    Trombone Ron wants to share his music with everyone.
    The music are bullets.

---
+++ conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+	t.title = "Trombone Ron"
+	t.author = "IC INDUSTRIES"
+	
+	t.identity = "trumbone-ron"
+	t.console = true
+	t.version = "0.9.1"
+	
+	t.screen.width = 800
+	t.screen.height = 600
+end
