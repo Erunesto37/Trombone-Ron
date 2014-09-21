7d65877902eb92de78d166755c8adfc4316c07ac
Doctor- <413Calliope413@gmail.com>
2014-09-20 19:33:07 -0700

    Deleted all of main.lua.
    I'm focusing on the trivial stuff right now, let's change that! Getting
    rid of all of main.lua and actually starting on the gameplay.

--- main.lua
+++ main.lua
@@ -1,14 +1,7 @@
 function love.load()
-	love.graphics.setBackgroundColor(0, 0, 0)
-	logo = love.graphics.newImage("logo.png")
-	startunpressed = love.graphics.newImage("startunpressed.png")
-	startpressed = love.graphics.newImage("startpressed.png")
-	font = love.graphics.newFont("terminal.fon", 12)
-	love.graphics.setFont(font)
 end
 
 function love.draw()
-	love.graphics.draw(startunpressed, 200, 400)
 end
 
 function love.update(dt)
